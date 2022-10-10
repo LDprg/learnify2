11f059905813287573a16b71bab7fc3ffbc4c5f6
LDprg <71488985+LDprg@users.noreply.github.com>
2022-10-10 20:00:45 +0200

    add home and profile page

--- src/pages/main/HomePage.tsx
+++ src/pages/main/HomePage.tsx
@@ -1,12 +1,62 @@
 import "../index.css";
+import { IonContent, IonGrid, IonCol, IonRow, IonHeader, IonItem, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton, IonIcon } from '@ionic/react';
 
 const HomePage: React.FC = () => {
-  return (
-    <div className="container">
-      <strong>Home Page</strong>
-      <p>Getting started here!</p>
-    </div>
-  );
+    return (
+        <IonContent>
+            <IonGrid>
+                <IonRow>
+                    <IonCol>
+                        <IonCard>
+                            <IonCardHeader>
+                                <IonCardTitle>
+                                    Welcome to the Learnify!
+                                </IonCardTitle>
+                                <IonCardSubtitle>
+                                    This is a learning app for everyone!
+                                </IonCardSubtitle>
+                            </IonCardHeader>
+                            <IonCardContent>
+                                <IonItem>
+                                    <IonCardTitle>
+                                        What is Learnify?
+                                    </IonCardTitle>
+                                </IonItem>
+                                <IonItem>
+                                    <IonCardSubtitle>
+                                        Learnify is a learning app for everyone. You can create your own sets and learn from other people's sets.
+                                    </IonCardSubtitle>
+                                </IonItem>
+                                <IonItem>
+                                    <IonCardTitle>
+                                        How to use Learnify?
+                                    </IonCardTitle>
+                                </IonItem>
+                                <IonItem>
+                                    <IonCardSubtitle>
+                                        You can create your own sets and learn from other people's sets.
+                                    </IonCardSubtitle>
+                                </IonItem>
+                                <IonItem>
+                                    <IonCardTitle>
+                                        How to support Learnify?
+                                    </IonCardTitle>
+                                </IonItem>
+                                <IonItem>
+                                    <IonCardSubtitle>
+                                        You can support Learnify by a little sponsorship.
+                                    </IonCardSubtitle>
+                                    <IonButton>
+                                        {/* <IonIcon ios={Patreon} md={userPages.mdIcon}></IonIcon> */}
+                                    </IonButton>
+                                </IonItem>
+                            </IonCardContent>
+                        </IonCard>
+                    </IonCol>
+                </IonRow>
+            </IonGrid>
+        </IonContent>
+    );
 };
 
 export default HomePage;
--- src/pages/user/ProfilePage.tsx
+++ src/pages/user/ProfilePage.tsx
@@ -1,12 +1,53 @@
 import "../index.css";
+import { IonCol, IonContent, IonGrid, IonRow, IonLabel, IonItem } from '@ionic/react';
+import { useGetUser } from "../../hooks/useApi";
+import { useLocation } from "react-router";
+import { useEffect } from "react";
 
 const ProfilePage: React.FC = () => {
-  return (
-    <div className="container">
-      <strong>Profile Page</strong>
-      <p>Getting started here!</p>
-    </div>
-  );
+    const location = useLocation();
+
+    const getUser = useGetUser();
+
+    console.log(getUser.data);
+
+    useEffect(() => {
+        if (location.pathname === "/Profile") {
+            getUser.request();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [location.pathname]);
+
+    return (
+        <IonContent>
+            <IonGrid>
+                <IonRow>
+                    <IonCol>
+                        <IonItem>
+                            <IonLabel>User:</IonLabel>
+                            <IonLabel>{getUser.data?.username}</IonLabel>
+                        </IonItem>
+                    </IonCol>
+                </IonRow>
+                <IonRow>
+                    <IonCol>
+                        <IonItem>
+                            <IonLabel>Email:</IonLabel>
+                            <IonLabel>{getUser.data?.email}</IonLabel>
+                        </IonItem>
+                    </IonCol>
+                </IonRow>
+                <IonRow>
+                    <IonCol>
+                        <IonItem>
+                            <IonLabel>UserId:</IonLabel>
+                            <IonLabel>{getUser.data?.id}</IonLabel>
+                        </IonItem>
+                    </IonCol>
+                </IonRow>
+            </IonGrid>
+        </IonContent>
+    );
 };
 
 export default ProfilePage;
