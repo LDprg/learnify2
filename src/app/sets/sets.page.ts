1709734cf299f02658f518145e760ea1861d7247
LDprg <71488985+LDprg@users.noreply.github.com>
2023-01-02 20:03:31 +0100

    start coding sets page

--- src/app/sets/sets.page.ts
+++ src/app/sets/sets.page.ts
@@ -6,6 +6,7 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./sets.page.scss'],
 })
 export class SetsPage implements OnInit {
+    public Sets: any = [];
 
     constructor() {
     }
