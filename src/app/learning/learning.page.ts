019ca011101ba55c616c5a7a38a38ca67b0c84e6
LDprg <71488985+LDprg@users.noreply.github.com>
2023-01-16 15:38:50 +0100

    start coding learning page

--- src/app/learning/learning.page.ts
+++ src/app/learning/learning.page.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {ApiService} from "../api.service";
+import {ViewWillEnter} from "@ionic/angular";
 
 export enum Status {
     Ask,
@@ -12,13 +14,31 @@ export enum Status {
     templateUrl: './learning.page.html',
     styleUrls: ['./learning.page.scss'],
 })
-export class LearningPage implements OnInit {
+export class LearningPage implements OnInit, ViewWillEnter {
     public id!: string;
     public starred: boolean = false;
 
     public status: Status = Status.Ask;
 
-    constructor(private activatedRoute: ActivatedRoute) {
+    public set : any;
+    public data : any;
+
+    public answerText: any;
+
+    constructor(private activatedRoute: ActivatedRoute, private apiService: ApiService) {
+    }
+
+    ionViewWillEnter() {
+
+        this.apiService.getSet(this.id).then((res) => {
+            this.set = res;
+
+
+            this.data = res.data;
+            this.data = this.shuffleArray(this.data);
+
+            console.log(this.data);
+        });
     }
 
     ngOnInit() {
@@ -26,4 +46,23 @@ export class LearningPage implements OnInit {
         this.starred = this.activatedRoute.snapshot.paramMap.get('starred') === 'true';
     }
 
+    checkAnswer() {
+        this.status = Status.Answer;
+    }
+
+    shuffleArray (value: any[]) {
+
+        let items: any[] = [];
+
+        for (let i = 0; i < value.length; i++) {
+            items.push(value[i]);
+        }
+
+        // reversing items array
+        for (let i = items.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [items[i], items[j]] = [items[j], items[i]];
+        }
+        return items;
+    }
 }
